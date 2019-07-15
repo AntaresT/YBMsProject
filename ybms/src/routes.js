8b42d5539f74df28d5d27bd690e8c93eabdc376a
AntaresT <luizeduardovbe@gmail.com>
2019-07-15 17:22:44 -0300

    Projeto Reajustado

---
+++ ybms/src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {BrowseRouter, Switch, Route} from 'react-router-dom';
+
+import Main from './pages/main';
+
+const Routes = () => (
+
+	<BrowseRouter>
+		<Switch>
+		
+		
+		</Switch>
+	</BrowseRouter>
+
+)
